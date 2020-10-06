9c342b8600f72f047e8d70524427ae947bfcf725
Daniel Souza <daniel.n10@hotmail.com>
2020-10-05 21:41:48 -0300

    final application

---
+++ precache-manifest.88e71bd31b58475301cbd3238ca8c959.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4c431f43fd47f3b1331c2f4cd0d85b7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dc33c5ec52e6b83fe50b",
+    "url": "/static/js/2.fedf20d5.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.fedf20d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "476de34ed4f6cb8cd08f",
+    "url": "/static/js/main.cc499626.chunk.js"
+  },
+  {
+    "revision": "250103716dfc89e5abb8",
+    "url": "/static/js/runtime-main.9ff9079d.js"
+  },
+  {
+    "revision": "a9f3834ed4cf42b5e54bdca63f7abc99",
+    "url": "/static/media/backgroundImg.a9f3834e.png"
+  }
+]);
